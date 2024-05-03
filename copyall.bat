a488b095c4bdc8d81abfd952ab534c015a9d45b7
Ferenc Pintér <pinterfer@gmail.com>
2024-05-03 11:50:04 +0200

    Release copy helper batch file for my convenience

---
+++ copyall.bat
@@ -0,0 +1,39 @@
+@REM mvtools2
+xcopy Sources\x64\Release\mvtools2.dll out\x64\MSVC\ /y
+xcopy Sources\x64\Release_v141_xp\mvtools2.dll out\x64\MSVC_XP\ /y
+xcopy Sources\x64\Rel_Clang\mvtools2.dll out\x64\ClangCL\ /y
+xcopy Sources\x64\ICX\mvtools2.dll out\x64\Intel_ICX_LLVM\ /y
+xcopy Sources\x64\ICL\mvtools2.dll out\x64\Intel_ICL_Classic\ /y
+
+xcopy Sources\Win32\Release\mvtools2.dll out\Win32\MSVC\ /y
+xcopy Sources\Win32\Release_v141_xp\mvtools2.dll out\Win32\MSVC_XP\ /y
+xcopy Sources\Win32\Rel_Clang\mvtools2.dll out\Win32\ClangCL\ /y
+xcopy Sources\Win32\ICX\mvtools2.dll out\Win32\Intel_ICX_LLVM\ /y
+xcopy Sources\Win32\ICL\mvtools2.dll out\Win32\Intel_ICL_Classic\ /y
+@REM depan
+xcopy DePan\x64\Release\DePan.dll out\x64\MSVC\ /y
+xcopy DePan\x64\Release_v141_xp\DePan.dll out\x64\MSVC_XP\ /y
+xcopy DePan\x64\Rel_Clang\DePan.dll out\x64\ClangCL\ /y
+xcopy DePan\x64\ICX\DePan.dll out\x64\Intel_ICX_LLVM\ /y
+xcopy DePan\x64\ICL\DePan.dll out\x64\Intel_ICL_Classic\ /y
+
+xcopy DePan\Win32\Release\DePan.dll out\Win32\MSVC\ /y
+xcopy DePan\Win32\Release_v141_xp\DePan.dll out\Win32\MSVC_XP\ /y
+xcopy DePan\Win32\Rel_Clang\DePan.dll out\Win32\ClangCL\ /y
+xcopy DePan\Win32\ICX\DePan.dll out\Win32\Intel_ICX_LLVM\ /y
+xcopy DePan\Win32\ICL\DePan.dll out\Win32\Intel_ICL_Classic\ /y
+
+@REM depanestimate
+xcopy DePanEstimate\x64\Release\DePanEstimate.dll out\x64\MSVC\ /y
+xcopy DePanEstimate\x64\Release_v141_xp\DePanEstimate.dll out\x64\MSVC_XP\ /y
+xcopy DePanEstimate\x64\Rel_Clang\DePanEstimate.dll out\x64\ClangCL\ /y
+xcopy DePanEstimate\x64\ICX\DePanEstimate.dll out\x64\Intel_ICX_LLVM\ /y
+xcopy DePanEstimate\x64\ICL\DePanEstimate.dll out\x64\Intel_ICL_Classic\ /y
+
+xcopy DePanEstimate\Win32\Release\DePanEstimate.dll out\Win32\MSVC\ /y
+xcopy DePanEstimate\Win32\Release_v141_xp\DePanEstimate.dll out\Win32\MSVC_XP\ /y
+xcopy DePanEstimate\Win32\Rel_Clang\DePanEstimate.dll out\Win32\ClangCL\ /y
+xcopy DePanEstimate\Win32\ICX\DePanEstimate.dll out\Win32\Intel_ICX_LLVM\ /y
+xcopy DePanEstimate\Win32\ICL\DePanEstimate.dll out\Win32\Intel_ICL_Classic\ /y
+
+
